fe43b11deafa330750bcae2a9d9c3e35391ddf62
SRSwebsite <srs.e.cart.app@gmail.com>
2025-09-15 00:23:25 +0330

    Delete redesign/part2/web directory

--- redesign/part2/web/file/menu.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementById('menuHeader');
-  const menu = document.getElementById('menuItems');
-  let open = false;
-
-  header.addEventListener('click', () => {
-    open = !open;
-    menu.style.maxHeight = open ? menu.scrollHeight + 'px' : '0';
-    header.textContent = open ? 'منوی اصلی پروژه ▲' : 'منوی اصلی پروژه ▼';
-  });
